ef1c5d1bd169e22fd92962a4443b85fc68f3299a
Alex <alex@jetweels.com>
2024-07-13 00:01:31 +0700

    on boarding form and add shadcn UI

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,9 @@
 import { ClerkProvider } from '@clerk/nextjs';
+import { dark } from '@clerk/themes';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import '../globals.css';
 
 export const metadata: Metadata = {
   title: 'Threads',
@@ -15,9 +18,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang='en'>
-        <body className={`${inter.className}`}>{children}</body>
+        <body className={`${inter.className} bg-dark-1`}>{children}</body>
       </html>
     </ClerkProvider>
   );
--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,8 +1,31 @@
+import AccountProfile from '@/components/forms/AccountProfile';
+
+import { currentUser, User } from '@clerk/nextjs/server';
+
 async function Page() {
+  const user: User | null = await currentUser();
+
+  const userInfo: any = {};
+
+  const userData = {
+    id: user?.id,
+    objectId: userInfo?._id,
+    username: userInfo?.username || user?.username,
+    fullName: userInfo?.name || user?.fullName,
+    bio: userInfo?.bio || '',
+    imageUrl: userInfo?.image || user?.imageUrl,
+  };
+
   return (
-    <main>
+    <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20 bg-dark-1'>
       <h1 className='head-text'>Onboarding</h1>
-      <p>Welcome to the app! Please sign in to continue.</p>
+      <p className='mt-3 text-base-regular text-light-2'>
+        Complete your profile now to use Threads
+      </p>
+
+      <section className='mt-9 bg-dark-2 p-10'>
+        <AccountProfile user={userData} btnTitle='Continue' />
+      </section>
     </main>
   );
 }
